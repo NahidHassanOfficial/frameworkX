ab8312b3c3f62a5b9dc64554ccac1f58d5cfd78f
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-08-22 22:07:37 +0600

    implemented RBAC middleware

---
+++ middlewares/RBACMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+class RBACMiddleware
+{
+    public static function allow($permissions = [])
+    {
+        $allowed = false;
+        foreach ($permissions as $permission) {
+            if (isNotEmpty($_SESSION, 'PERMISSIONS')) {
+                if (in_array($permission, $_SESSION['PERMISSIONS'])) {
+                    $allowed = true;
+                } else {
+                    $allowed = false;
+                    break;
+                }
+            }
+        }
+        if ($allowed) {
+            return true;
+        } else {
+            Response::failed('You do not have permission to access this resource', 403);
+            exit; // prevent further script execution
+        }
+    }
+}
