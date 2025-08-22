818da1434338bf96906a797f92f21297f8110190
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-08-22 22:10:16 +0600

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 return [
-    // [METHOD, ROUTE, [Controller, Method], Middleware]
+    // [METHOD, ROUTE, [Controller, Method], [Middleware::method:params]]
     'prefix' => '/',
     'routes' => [
         ['GET', '/', ['HomeController', 'index']],
-        ['GET', '/about', ['HomeController', 'about']],
-    ]
+        ['GET', '/profile/{id}/{param}', ['HomeController', 'profile'], 'AuthMiddleware::check'],
+        ['GET', '/about', ['HomeController', 'about'], ['AuthMiddleware::check', 'RBACMiddleware::allow:[123,133]']],
+        ['GET', '/404', function () {
+            return View::render('error/404');
+        }],
+    ],
 ];
