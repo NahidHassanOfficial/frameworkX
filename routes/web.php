1940ec88b71f10d56ba7abdbb4927eebb2284d6a
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2025-08-27 11:57:34 +0600

    fixed 404 route view argument

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ return [
         ['GET', '/profile/{id}/{param}', ['HomeController', 'profile'], 'AuthMiddleware::check'],
         ['GET', '/about', ['HomeController', 'about'], ['RBACMiddleware::allow:123,133']],
         ['GET', '/404', function () {
-            return View::render('error/404');
+            return View::render('error/404', [], null);
         }],
     ],
 ];
